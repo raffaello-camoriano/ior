865ac8bc30bd0966373474afb3a90dec0b700b63
Raffaello Camoriano <raffaello.camoriano@iit.it>
2015-08-30 16:37:57 -0400

    1st working version of realistic incremental RLSC. TODO: Rebalancing, check validation set at step k, use real images

--- main.m
+++ main.m
@@ -3,16 +3,16 @@ clearAllButBP;
 close all;
 
 %% Experiments setup
-run_batch = 1;
-run_incremental_balanced_parallel = 1;
-run_batch_realistic_rebalanced_loss = 1;
+run_batch = 0;
+run_incremental_balanced_parallel = 0;
+run_batch_realistic_rebalanced_loss = 0;
 run_incremental_realistic_rebalanced_label = 1;
 
 %% Load data
 
 % Random dataset
 
-ntr = 1000;     % Number of training samples
+ntr = 4000;     % Number of training samples
 nte = 1000;     % Number of test samples
 n = ntr + nte;  % Total number of examples
 d = 100;        % Dimensionality
@@ -37,6 +37,34 @@ Xtr = X(1:ntr,:);
 Xte = X(ntr+1:ntr+nte,:);
 Ytr = Y(1:ntr,:);
 Yte = Y(ntr+1:ntr+nte,:);
+
+% Format dataset in class-specific cells
+
+X_c = cell(1,t);
+Y_c = cell(1,t);    
+Xtr_c = cell(1,t);
+Ytr_c = cell(1,t);    
+Xte_c = cell(1,t);
+Yte_c = cell(1,t);
+
+for sampleidx = 1:n
+    for classidx = 1:t
+        if Y(sampleidx,classidx) == 1
+%                 Y_c{classidx}(sampleidx,:) = Y(sampleidx,:);
+%                 X_c{classidx}(sampleidx,:) = X(sampleidx,:);
+            Y_c{classidx} = [ Y_c{classidx} ; Y(sampleidx,:)];
+            X_c{classidx} = [ X_c{classidx} ; X(sampleidx,:)];
+%                 break
+        end
+    end
+end
+
+for classidx = 1:t
+    Xtr_c{classidx} = X_c{classidx}(1:(ntr/t),:);
+    Ytr_c{classidx} = Y_c{classidx}(1:(ntr/t),:);    
+    Xte_c{classidx} = X_c{classidx}((ntr/t)+1:(ntr+nte)/t,:);
+    Yte_c{classidx} = Y_c{classidx}((ntr/t)+1:(ntr+nte)/t,:);
+end
 
 %% Batch RLSC
 % Naive Linear Regularized Least Squares Classifier, 
@@ -144,7 +172,7 @@ nlambda = 8;
 
 if run_incremental_balanced_parallel == 1
 
-    lrng = logspace(1 , -6 , nlambda);    % Lambda range
+    lrng = logspace(1 , -6 , nlambda);              % Lambda range
     numUpdates = floor(ntr / (nuptr + nupval));     % Number of update steps
     
     R = cell(1,nlambda);
@@ -256,7 +284,7 @@ if run_incremental_balanced_parallel == 1
         end
     end
     
-    %% Plots
+    % Plots
     
     % Test Accuracy evolution for different lambdas
     figure
@@ -289,33 +317,177 @@ end
 
 
 %% Incremental realistic with rebalancing via label reweighting
-if run_incremental_realistic_rebalanced_label == 1
-    
-    %% Format dataset in class-specific cells
-    
-    X_c = cell(1,t);
-    Y_c = cell(1,t);    
-    Xtr_c = cell(1,t);
-    Ytr_c = cell(1,t);    
-    Xte_c = cell(1,t);
-    Yte_c = cell(1,t);
 
-    for sampleidx = 1:n
-        for classidx = 1:t
-            if Y(sampleidx,classidx) == 1
-%                 Y_c{classidx}(sampleidx,:) = Y(sampleidx,:);
-%                 X_c{classidx}(sampleidx,:) = X(sampleidx,:);
-                Y_c{classidx} = [ Y_c{classidx} ; Y(sampleidx,:)];
-                X_c{classidx} = [ X_c{classidx} ; X(sampleidx,:)];
-%                 break
+if run_incremental_realistic_rebalanced_label == 1
+
+    % Configuration
+    nuptr = 80;     % Number of training examples per update
+    nupval = 20;    % Number of validation examples per update
+    nlambda = 8;
+    numUpdates = floor(ntr / ((nuptr + nupval)*t) );     % Number of update steps
+    lrng = logspace(1 , -6 , nlambda);              % Lambda range
+
+    %  Objects classes are presented in random order
+    classSequence = randperm(t);
+    
+    % Reformat dataset
+    Ytr_c1 = cell(1,t);
+    Yte_c1 = cell(1,t);
+    for cidx = 1:t
+        c = classSequence(cidx);
+        
+        %Training set
+        Ytr_c1{cidx} = zeros(ntr/t,cidx);
+        Ytr_c1{cidx}(:,cidx) = ones(ntr/t,1);
+        
+        %Test set
+        Yte_c1{cidx} = zeros(ntr/t,cidx);
+        Yte_c1{cidx}(:,cidx) = ones(ntr/t,1);
+    end
+    
+    
+    R = cell(1,nlambda);
+    testAcc = cell(1,nlambda);  % Test accuracy for each update
+    lstarqueue = [];
+    bestAccqueue = [];
+    Xte_tmp = [];
+    Yte_tmp = [];
+    
+    for cidx = 1:t
+        
+        c = cidx;
+        currUpIdx = 1;  % Current index of the first example of the update minibatch
+        
+        for k = 1:numUpdates
+
+            % Get data chunks
+            Xuptr = Xtr_c{c}(currUpIdx:(currUpIdx+nuptr-1),:);
+            Yuptr = Ytr_c1{c}(currUpIdx:(currUpIdx+nuptr-1),:);
+            Xupval = Xtr_c{c}(currUpIdx+nuptr:(currUpIdx+nuptr+(k*nupval)-1),:);
+            Yupval = Ytr_c1{c}(currUpIdx+nuptr:(currUpIdx+nuptr+(k*nupval)-1),:);
+
+            currUpIdx = currUpIdx + nuptr;
+
+            if k == 1 && cidx == 1
+                % Compute cov mat XtX and b (XtY)
+                XtX = Xuptr'*Xuptr;
+                XtY = Xuptr'*Yuptr;   
+            elseif k == 1
+                % Attach new column to XtY and update it with incoming
+                % examples
+                XtY = [XtY , zeros(d,1)] + Xuptr' * Yuptr;
+            else
+                % Update XtY with the incoming examples
+                XtY = XtY + Xuptr' * Yuptr;
+            end
+            
+            % Extend incremental test set to the new class
+            if k == 1 && cidx == 1
+                Xte_tmp = [ Xte_tmp ; Xte_c{c} ];
+                Yte_tmp = [ Yte_tmp ; Yte_c1{c} ];
+            elseif k == 1
+                Xte_tmp = [ Xte_tmp ; Xte_c{c} ];
+                Yte_tmp = [ Yte_tmp , zeros(size(Yte_tmp,1),1) ];   % Add a comumn of zeros
+                Yte_tmp = [ Yte_tmp ; Yte_c1{c} ];  % Add new test samples labels
+            end
+
+            % Initialize model selection vars
+            lstar = max(lrng);      % Best lambda
+            bestAcc = 0;    % Highest accuracy    
+
+            for lidx = 1:nlambda
+
+                l = lrng(lidx);
+
+                if k == 1
+                    % Compute first Cholesky factorization of XtX + n * lambda * I
+                    R{lidx} = chol(XtX + nuptr * l * eye(d),'upper');  
+                else
+                    % Update Cholesky factor
+                    R{lidx} = cholupdatek(R{lidx},Xuptr);                
+                end
+
+               %% Training
+                w = R{lidx} \ (R{lidx}' \ XtY );
+
+               %% Validation
+                % Predict validation labels
+                Yupvalpred_raw = Xupval * w;
+
+                % Encode output
+                Yupvalpred = zeros(k*nupval,cidx);
+                for i = 1:nupval
+                    [~,maxIdx] = max(Yupvalpred_raw(i,:));
+                    Yupvalpred(i,maxIdx) = 1;
+                end
+                clear Yupvalpred_raw;
+
+                % Compute current accuracy
+                C = transpose(bsxfun(@eq, Yupval', Yupvalpred'));
+                D = sum(C,2);
+                E = D == cidx;
+                numCorrect = sum(E);
+                currAcc = (numCorrect / (k*nupval));     
+
+                if currAcc > bestAcc
+                    bestAcc = currAcc;
+                    lstar = l;
+                end
+            end
+
+            lstarqueue = [lstarqueue , lstar];
+            bestAccqueue = [bestAccqueue , bestAcc];
+
+           %% Testing
+
+            for lidx = 1:nlambda
+
+                l = lrng(lidx);
+
+                % Predict test labels
+                Ytepred_raw = Xte * w;
+
+                % Encode output
+                Ytepred = zeros(nte,t);
+                for i = 1:nte
+                    [~,maxIdx] = max(Ytepred_raw(i,:));
+                    Ytepred(i,maxIdx) = 1;
+                end
+                clear Yuptestpred_raw;
+
+                % Compute accuracy
+                C = transpose(bsxfun(@eq, Yte', Ytepred'));
+                D = sum(C,2);
+                E = D == cidx;
+                numCorrect = sum(E);
+                testAcc{lidx} = [testAcc{lidx} , (numCorrect / nte)];
             end
         end
     end
     
-    for classidx = 1:t
-        Xtr_c{classidx} = X_c{classidx}(1:(ntr/t),:);
-        Ytr_c{classidx} = Y_c{classidx}(1:(ntr/t),:);    
-        Xte_c{classidx} = X_c{classidx}((ntr/t)+1:(ntr+nte)/t,:);
-        Yte_c{classidx} = Y_c{classidx}((ntr/t)+1:(ntr+nte)/t,:);
-    end
+
+    % Plots
+    
+    % Test Accuracy evolution for different lambdas
+    figure
+    mesh(cell2mat(testAcc'))
+    title('Test Accuracy evolution for different \lambda')
+    xlabel('k')    
+    ylabel('\lambda')
+    zlabel('Accuracy')    
+
+    % Best lambdas evolution
+    figure
+    semilogy(lstarqueue)
+    title('\lambda* evolution for different \lambda')
+    xlabel('k')    
+    ylabel('\lambda*')
+    
+    % Best accuracies evolution
+    figure
+    plot(bestAccqueue)
+    title('Best validation accuracies evolution for different \lambda')
+    xlabel('k')    
+    ylabel('Best validation accuracies')    
+    
 end
